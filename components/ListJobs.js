be8f0ab4f8a80cb2cdf240fd4ebf6f2986ddfb62
Flavio Lima <flavioislima@gmail.com>
2018-10-02 09:37:58 -0300

    Added Async Storage to handle favorites

--- components/ListJobs.js
+++ components/ListJobs.js
@@ -1,25 +1,52 @@
 import React from 'react'
-import { StatusBar, View, FlatList, Text, RefreshControl, TextInput, TouchableOpacity, Linking, StyleSheet } from 'react-native'
-import { SearchBar } from 'react-native-elements'
+import { StatusBar, View, FlatList, Text, RefreshControl, AsyncStorage, StyleSheet, Alert, NetInfo, ToastAndroid } from 'react-native'
+import { SearchBar, Button } from 'react-native-elements'
 import axios from 'axios'
 import Jobs from './Jobs'
 
 export default class ListJobs extends React.Component {
     state = {
         data: [],
+        favorites: [],
         filterText: '',
         refreshing: true
     }
 
     _onGetData = async () => {
-        this.setState({ refreshing: true })
-        await axios.get(this.props.url)
-            .then(data => {
-                this.setState({
-                    data: data.data,
-                    refreshing: false
+        if (NetInfo.isConnected) {
+            await axios.get(this.props.url)
+                .then(data => {
+                    this.setState({
+                        data: data.data,
+                        refreshing: false
+                    })
                 })
+        } else {
+            ToastAndroid.showWithGravity('Network Error', 4500, ToastAndroid.CENTER)
+        }
+        let keys = []
+        await AsyncStorage.getAllKeys()
+            .then(async (data) => {
+                keys = await data
+                this.setState({ favorites: keys, refreshing: false })
             })
+    }
+
+    _getStorageData = async () => {
+        let favs = []
+        await AsyncStorage.getAllKeys()
+            .then((data) => {
+                if (data.length > 0) {
+                    data.map(async (key) => await AsyncStorage.getItem(key)
+                        .then(fav => favs.push(JSON.parse(fav)))
+                        .then(() => this.setState({ data: favs, refreshing: false }))
+                        .catch(err => console.error(err))
+                    )
+                } else {
+                    this.setState({ refreshing: false })
+                }
+            })
+            .catch(err => alert("Error: ", err))
     }
 
     _onSearchJobs = (filterText) => {
@@ -30,12 +57,34 @@ export default class ListJobs extends React.Component {
         this.setState({ filterText: '' })
     }
 
-    _onRefresh = () => {
-        this._onGetData()
+    _onRefresh = async () => {
+        // this.setState({ data: [], favorites: [] })
+        if (this.props.url) {
+            await this._onGetData()
+        } else {
+            await this._getStorageData()
+        }
+    }
+
+    _clearFavorites = () => {
+        AsyncStorage.clear()
+            .then(() => {
+                this.setState({ favorites: [], data: [] })
+                Alert.alert('Erase Favorites', 'Favorites Cleared!', [{ text: 'OK' }])
+            })
+            .catch(res => console.error(res))
+    }
+
+    _handleClearFavorites = () => {
+        Alert.alert('Erase Favorites', 'Are You Sure?', [{ text: 'Cancel' }, { text: 'Yes', onPress: this._clearFavorites }])
     }
 
     async componentDidMount() {
-        await this._onGetData()
+        if (this.props.url) {
+            await this._onGetData()
+        } else {
+            await this._getStorageData()
+        }
     }
 
     render() {
@@ -74,9 +123,14 @@ export default class ListJobs extends React.Component {
                     style={styles.container}
                     data={filteredData}
                     renderItem={(job) => {
-                        if (job.item.id || job.item.jobId) {
+                        let { id, jobId } = job.item
+                        id = id ? id : jobId
+                        if (this.state.favorites.includes(id)) job.item.isFavorite = true
+                        if (jobId) job.item.id = String(jobId)
+                        if (id) {
                             return (
                                 <Jobs
+                                    refresh={this._onRefresh}
                                     source={this.props.source}
                                     data={job.item} />
                             )
@@ -91,6 +145,14 @@ export default class ListJobs extends React.Component {
                         />
                     }
                 />
+                {this.props.source === 'Favorites' &&
+                    <Button
+                        transparent
+                        color='red'
+                        leftIcon={{ name: 'trash', type: 'font-awesome', color: 'red' }}
+                        title="Clear Favorites"
+                        onPress={this._handleClearFavorites} />
+                }
             </View >
         )
     }
