f6beaba2fa24d9d811087492f32f492def303041
Flavio F Lima <flavioislima@gmail.com>
2019-04-16 11:58:47 +0200

    Updated icon for IOS

--- ios/remoteworkTests/remoteworkTests.m
+++ ios/remoteworkTests/remoteworkTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface remoteworkTests : XCTestCase
+@interface RemoteWorkTests : XCTestCase
 
 @end
 
-@implementation remoteworkTests
+@implementation RemoteWorkTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
