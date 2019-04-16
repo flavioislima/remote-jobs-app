f0720819d9824199ae22978fbb6e2283b873ebcf
Flavio F Lima <flavioislima@gmail.com>
2019-04-16 10:48:54 +0200

    Updated RN to 0.59.4

--- android/app/src/main/java/com/remotework/MainApplication.java
+++ android/app/src/main/java/com/remotework/MainApplication.java
@@ -3,7 +3,7 @@ package com.remotework;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.sbugert.rnadmob.RNAdMobPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNAdMobPackage(),
+            new RNGestureHandlerPackage(),
             new VectorIconsPackage()
       );
     }
