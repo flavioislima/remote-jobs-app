17199186482e6a14e2385cbf203e914b4c0e4884
Flavio F Lima <flavioislima@gmail.com>
2019-04-16 13:02:57 +0200

    Removed AdBanner

--- src/screens/Browser.js
+++ src/screens/Browser.js
@@ -1,24 +1,30 @@
-import React from 'react'
-import { View, WebView, TouchableOpacity, Text, Linking, StyleSheet } from 'react-native'
+import React from 'react';
+import {
+  View,
+  WebView,
+  TouchableOpacity,
+  Text,
+  Linking,
+  StyleSheet
+} from 'react-native';
 
-import AdBanner from '../UI/AdBanner'
+// import AdBanner from '../UI/AdBanner';
 
-export default class Browser extends React.Component {
+export default class BrowserScreen extends React.Component {
   static navigationOptions = {
     header: null
-  }
+  };
 
   render() {
     const { navigation } = this.props;
     const url = navigation.getParam('url', 'http://google.com');
     return (
       <View style={{ flex: 1 }}>
-        <TouchableOpacity style={styles.touch}
+        <TouchableOpacity
+          style={styles.touch}
           onPress={() => Linking.openURL(url)}
         >
-          <Text style={styles.text}>
-            Open On External Browser
-        </Text>
+          <Text style={styles.text}>Open On External Browser</Text>
         </TouchableOpacity>
         <WebView
           javaScriptEnabled
@@ -30,13 +36,19 @@ export default class Browser extends React.Component {
           style={{ marginVertical: 5 }}
         />
 
-        <AdBanner />
+        {/* <AdBanner /> */}
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   text: { fontSize: 16, fontWeight: '700', color: 'white' },
-  touch: { height: 32, backgroundColor: '#0dbc79', justifyContent: 'center', alignItems: 'center', alignContent: 'center' },
-})
+  touch: {
+    height: 32,
+    backgroundColor: '#0dbc79',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center'
+  }
+});
