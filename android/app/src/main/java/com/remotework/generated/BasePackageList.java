1f9b1862d532eceec1fc7f46932193238267672d
Flavio F Lima <flavioislima@gmail.com>
2019-08-28 19:33:59 +0200

    Added some expo packages

---
+++ android/app/src/main/java/com/remotework/generated/BasePackageList.java
@@ -0,0 +1,16 @@
+package com.remotework.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.ads.admob.AdMobPackage(),
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.permissions.PermissionsPackage()
+    );
+  }
+}
