9302106e5886c156f8773836cd819096e1faf215
Flavio F Lima <flavioislima@gmail.com>
2019-09-03 10:21:14 +0200

    Removed Rating Button

--- src/components/Listjobs/ListJobs.tsx
+++ src/components/Listjobs/ListJobs.tsx
@@ -10,10 +10,8 @@ import { SafeAreaView } from "react-navigation";
 
 import { JobType } from "../../types";
 import Search from "../../UI/Search";
-import Stars from "../../UI/Stars";
 import Error from "../../UI/Error";
 import StatusJobs from "../../UI/StatusJobs";
-import RatingModal from "../../UI/RatingModal";
 import Job from "./Jobs/Job";
 import AsyncStorage from "@react-native-community/async-storage";
 
@@ -31,20 +29,11 @@ const ListJobs: React.FC<Props> = (props: Props) => {
   const { jobs, refreshing, refresh, error } = props;
 
   const [filterText, setFilterText] = useState("");
-  const [isModalVisible, setModalVisible] = useState(false);
   const [isRated, setRated] = useState(false);
 
   const filterRegex: RegExp = new RegExp(String(filterText), "i");
   const filter = (item: JobType) => filterRegex.test(item.position);
   const filteredData: JobType[] = jobs.filter(filter);
-
-  const handleRated = async (rated: string) => {
-    await AsyncStorage.setItem("rated", rated).then(async () => {
-      await AsyncStorage.getItem("rated").then(() => {
-        setRated(true);
-      });
-    });
-  };
 
   const renderJobs = (job: any) => {
     const { refresh, navigate } = props;
@@ -61,25 +50,11 @@ const ListJobs: React.FC<Props> = (props: Props) => {
     <View style={styles.container}>
       <StatusBar backgroundColor="#112038" />
       <SafeAreaView>
-        <View style={styles.status}>
-          <Stars
-            isRated={isRated}
-            modalVisible={isModalVisible}
-            setModalVisible={setModalVisible.bind(this, !isModalVisible)}
-          />
-          {isModalVisible && (
-            <RatingModal
-              rated={handleRated}
-              modalVisible={isModalVisible}
-              setModalVisible={setModalVisible}
-            />
-          )}
-        </View>
+        <Search
+          onChangeText={setFilterText}
+          onClearText={setFilterText.bind(this, "")}
+        />
       </SafeAreaView>
-      <Search
-        onChangeText={setFilterText}
-        onClearText={setFilterText.bind(this, "")}
-      />
       {error && <Error />}
       <StatusJobs
         refreshing={refreshing}
@@ -103,13 +78,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FFFFFF"
-  },
-  contentContainer: {
-    paddingTop: 0
-  },
-  status: {
-    backgroundColor: "#112038",
-    paddingTop: 0
   }
 });
 
