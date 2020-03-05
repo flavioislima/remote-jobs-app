d24335a746804477a4e29d97fdcaf9ab3d333fa6
Flavio F Lima <flavioislima@gmail.com>
2020-03-05 16:00:18 +0100

    feat: added firebase

--- src/UI/AdBanner.tsx
+++ src/UI/AdBanner.tsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { AdMobBanner } from "expo-ads-admob";
+import { BannerAd } from '@react-native-firebase/admob'
+import React from 'react'
 
-const unitId = "ca-app-pub-4477713466828746/3523474164";
+const unitId = 'ca-app-pub-4477713466828746/3523474164'
+// const AdMobBanner = BannerAd
 
 export default () => (
-  <AdMobBanner
-    bannerSize="fullBanner"
-    adUnitID={unitId} // Test ID, Replace with your-admob-unit-id
-    testDeviceID="EMULATOR"
+  <BannerAd
+    unitId={unitId}
+    size={'FULL_BANNER'}
+    requestOptions={{
+      requestNonPersonalizedAdsOnly: true
+    }}
+    onAdLoaded={() => {
+      console.log('Advert loaded');
+    }}
+    onAdFailedToLoad={(error) => {
+      console.error('Advert failed to load: ', error);
+    }}
   />
-);
+  )
