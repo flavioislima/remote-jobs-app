f731025bcf4ef3b099632a25b281028f32cd1844
Flavio F Lima <flavioislima@gmail.com>
2020-04-05 10:45:01 +0200

    Fix Build

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export enum adUnitIds {
+  JOBS = 'ca-app-pub-4477713466828746/3523474164',
+  SQUARE = 'ca-app-pub-4477713466828746/8984919217',
+  FAVORITES = 'ca-app-pub-4477713466828746/2838383079'
+}
+
+export enum adSizes {
+  SMART = 'SMART_BANNER',
+  RECTANGLE = 'MEDIUM_RECTANGLE',
+  SMALL = 'BANNER'
+}
+
+export const endpoint = 'https://us-central1-remote-work-br.cloudfunctions.net/getRemoteJobs'
